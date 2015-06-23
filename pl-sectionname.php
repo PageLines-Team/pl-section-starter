eb29a627845fbb5f45a58824412b344c578ad636
Raymond Lopez <ray@pagelines.com>
2015-06-23 12:37:34 -0500

    section skeleton

---
+++ pl-sectionname.php
@@ -0,0 +1,32 @@
+<?php
+/*
+	Plugin Name: 	PageLines Section SectionName
+	Description: 	Section description.
+	Author: 			PageLines
+	Version: 			1.0
+	PageLines: 		PLSectionNamePlugin
+	Filter: 			localsocial
+*/
+
+if( ! class_exists('PageLinesSection') )
+	return;
+
+class PLSectionNamePlugin extends PageLinesSection {
+
+	function section_persistent(){
+
+	}
+
+	function section_styles(){
+
+	}
+
+	function section_opts(){
+
+	}
+  
+  function section_template(){
+
+	}
+
+}
